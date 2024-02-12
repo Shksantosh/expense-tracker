9be48e878e93b55466b958599b8a632a820fc2e8
shksantosh <santoshking82@gmail.com>
2024-02-12 16:11:22 +0530

    Display Balance, Income & Expense

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
+  { id: 1, text: 'Flower', amount: -200 },
+  { id: 2, text: 'Salary', amount: 30000 },
+  { id: 3, text: 'Book', amount: -100 },
+  { id: 4, text: 'Camera', amount: 25000 }
 ];
 
 let transactions = dummyTransactions
@@ -36,11 +36,33 @@ function addTransactionDOM(transaction) {
 
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+    
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+      ).toFixed(2);
+    
+      balance.innerText = `₹${total}`;
+      money_plus.innerText = `₹${income}`;
+      money_minus.innerText = `₹${expense}`;
+}
+
 // Init app
 function init() {
     list.innerHTML = '';
   
-    transactions.forEach(addTransactionDOM);    
+    transactions.forEach(addTransactionDOM); 
+    updateValues()   
   }
   
   init();
